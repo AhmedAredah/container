66be7326baf75c8269f59977977fdd47a3a728ea
Ahmed Aredah <Ahmed.Aredah@gmail.com>
2024-10-21 13:43:48 -0400

    fix

--- src/containerpy/setup.py
+++ src/containerpy/setup.py
@@ -2,29 +2,29 @@ import os
 from setuptools import setup, find_packages
 from glob import glob
 
-def run():
-    # Define the package directory
-    package_dir = './ContainerPy'
 
-    # Collect all necessary files from the package directory
-    package_data_files = glob(f"{package_dir}/*")
+# Define the package directory
+package_dir = './ContainerPy'
 
-    # Install script
-    setup(
-        name="ContainerPy",
-        version="0.0.1",
-        author="Ahmed Aredah",
-        author_email="AhmedAredah@vt.edu",
-        description=("Modeling Containers on CargoNetSim simulator."),
-        license="View License on GitHub: 'https://github.com/AhmedAredah/container'",
-        keywords="Container library for CargoNetSim simulator",
-        packages=['ContainerPy'],
-        package_dir={'ContainerPy': package_dir},
-        package_data={'ContainerPy': [os.path.basename(f) for f in package_data_files]},
-        classifiers=[
-            "Development Status :: 0.0.1 - Alpha",
-            "Topic :: Utilities",
-            "License :: Review License on GitHub",
-        ],
-        include_package_data=True,
-    )
+# Collect all necessary files from the package directory
+package_data_files = glob(f"{package_dir}/*")
+
+# Install script
+setup(
+    name="ContainerPy",
+    version="0.0.1",
+    author="Ahmed Aredah",
+    author_email="AhmedAredah@vt.edu",
+    description=("Modeling Containers on CargoNetSim simulator."),
+    license="View License on GitHub: 'https://github.com/AhmedAredah/container'",
+    keywords="Container library for CargoNetSim simulator",
+    packages=['ContainerPy'],
+    package_dir={'ContainerPy': package_dir},
+    package_data={'ContainerPy': [os.path.basename(f) for f in package_data_files]},
+    classifiers=[
+        "Development Status :: 0.0.1 - Alpha",
+        "Topic :: Utilities",
+        "License :: Review License on GitHub",
+    ],
+    include_package_data=True,
+)
