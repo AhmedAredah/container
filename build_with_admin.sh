02ee45d779f5f244f01af28353f7c1066d51cd62
Ahmed Aredah <Ahmed.Aredah@gmail.com>
2024-11-13 23:51:39 -0500

    add macos support

--- build_with_admin.sh
+++ build_with_admin.sh
@@ -17,16 +17,28 @@ rm -rf build
 mkdir build
 cd build
 
-# Set the Qt path
-QT_PATH="/home/ahmed/Qt/6.8.0/gcc_64/lib/cmake/Qt6"
-PYTHON_PATH="/home/ahmed/miniconda3/envs/cargonetsim/bin/python"
+# Set paths based on the operating system
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    # Linux paths
+    QT_PATH="/home/ahmed/Qt/6.8.0/gcc_64/lib/cmake/Qt6"
+    PYTHON_PATH="/home/ahmed/miniconda3/envs/cargonetsim/bin/python"
+    NUM_CORES=$(nproc)
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    # macOS paths
+    QT_PATH="/Users/ahmedaredah/Qt/6.8.0/macos/lib/cmake/Qt6"
+    PYTHON_PATH="/opt/anaconda3/envs/cargonetsim/bin/python"
+    NUM_CORES=$(sysctl -n hw.ncpu)
+else
+    echo "Unsupported operating system."
+    exit 1
+fi
 
 # Configure and build in Debug mode
 cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_PREFIX_PATH="$QT_PATH" -DPYTHON_EXECUTABLE="$PYTHON_PATH"
-make install -j$(nproc)
+make install -j$NUM_CORES
 
 # Configure and build in Release mode
 cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="$QT_PATH" -DPYTHON_EXECUTABLE="$PYTHON_PATH"
-make install -j$(nproc)
+make install -j$NUM_CORES
 
 echo "Build process completed."
